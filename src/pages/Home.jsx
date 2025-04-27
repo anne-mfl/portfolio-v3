f16bddc6f6bfd10fbd475a486fbfbc0fd08e300c
anne <anne.gemma.lee@gmail.com>
2025-04-27 18:54:08 +0200

    tailwind and router set

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div className='text-3xl text-scarlet font-gilroy font-thin'>100</div>
+    <div className='text-3xl text-scarlet font-gilroy font-extralight'>200</div>
+    <div className='text-3xl text-scarlet font-gilroy font-light'>300</div>
+    <div className='text-3xl text-scarlet font-gilroy font-normal'>400</div>
+    <div className='text-3xl text-scarlet font-gilroy font-medium'>500</div>
+    <div className='text-3xl text-scarlet font-gilroy font-semibold'>600</div>
+    <div className='text-3xl text-scarlet font-gilroy font-bold'>700</div>
+    <div className='text-3xl text-scarlet font-gilroy font-extrabold'>800</div>
+    <div className='text-3xl text-scarlet font-gilroy font-black'>900</div>
+    </>
+  )
+}
+
+export default Home
