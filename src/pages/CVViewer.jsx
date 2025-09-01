07dcef33bdf439d6cbf6efa508de66c552af8cb2
anne <anne.gemma.lee@gmail.com>
2025-09-01 10:00:15 +0200

    fix path to CV

--- src/pages/CVViewer.jsx
+++ src/pages/CVViewer.jsx
@@ -5,7 +5,6 @@ const CVViewer = () => {
     <div style={{ width: '100dvw', height: '100dvh', margin: 0, padding: 0 }}>
       <embed 
         src='/Anne_Lee_CV.pdf' 
-        // src={`Anne_Lee_CV.pdf`} 
         type="application/pdf" 
         width="100%" 
         height="100%" 
